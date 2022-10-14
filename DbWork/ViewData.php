2b275942b79e00bcef7f9d99348505e3f72e61bc
Muneebakhan <muneebakhan430@gmail.com>
2022-10-14 12:35:01 +0500

    Updated Work 14-10-22

--- DbWork/ViewData.php
+++ DbWork/ViewData.php
@@ -5,10 +5,31 @@
 
 <?php
 $query = 'select * from student';
+if (isset($_GET['srch'])) {
+    $search = $_GET['search'];
+    $query = "select * from student where StduentName like '%$search%' or City like '%$search%' ";
+}
 $res = mysqli_query($con, $query); // $data = mysqli_fetch_assoc($res);
 $rowCount = mysqli_num_rows($res);
 echo $rowCount;
+
 // print_r($data);
+?>
+
+<form action = "" method = "get">
+                <div class="input-group col-lg-6 container mb-5 mt-3">
+                    <input type="text" class="form-control" placeholder="Search" name = "search">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-secondary" name = "srch" >Search</button>
+
+                        <a href="ViewData.php" class = "btn btn-primary ml-3">Reset</a>
+                    </div>
+                        
+                </div>
+                
+            </form>
+
+<?php
 if ($rowCount > 0) { ?>
 
 
@@ -36,7 +57,13 @@ if ($rowCount > 0) { ?>
             <td><?= $data['Fees'] ?></td>
             <td><?= $data['Courses'] ?></td>
 
-            <td><a href="Edit.php?id=<?= $data['StdId'] ?>" class = "btn btn-primary">Edit</a></td>
+            <td><a href="Edit.php?id=<?= $data[
+                'StdId'
+            ] ?>" class = "btn btn-primary">Edit</a></td>
+
+             <td><a href="ViewData.php?Delid=<?= $data[
+                 'StdId'
+             ] ?>" class = "btn btn-danger" onclick = "return confirm('Are you sure you want to delete!!'); return false;">Delete</a></td>
 
 
         <?php echo '</tr>';
@@ -46,6 +73,13 @@ if ($rowCount > 0) { ?>
 
 
 <?php } else {echo '<p>No Record Found</p>';}
+error_reporting(0);
+$DelID = $_GET['Delid'];
+$quer = "delete from student where StdId = $DelID";
+$res = mysqli_query($con, $quer);
+if ($res) {
+    echo "<script>alert('Data Deleted!!');window.location.href = 'ViewData.php';</script>";
+}
 ?>
 
  <?php include 'footer.php'; ?>
